30d3d92cad9c54604a5a3029e40619887341a95b
TaeGon-Go <70566366+TaeGon-Go@users.noreply.github.com>
2020-09-08 20:39:52 +0900

    Add files via upload

---
+++ BooleanApp.java
@@ -0,0 +1,20 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("One"); // String 문자열
+		System.out.println(1); // 정수 데이터 타입 int
+ 		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello world";
+		// String true = "Hello world"; // reserved word
+		
+		System.out.println(foo.contains("world")); //contains 그 문자열에 입력값으로 전달할 어떤 값이 들어있다면 ture 없다면 false
+		System.out.println(foo.contains("Croco"));
+
+	}
+
+}
