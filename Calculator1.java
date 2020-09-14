5d97cba1df6a6802882da2eed261a6067b6c3267
TaeGon-Go <70566366+TaeGon-Go@users.noreply.github.com>
2020-09-14 15:38:16 +0900

    Add files via upload

---
+++ Calculator1.java
@@ -0,0 +1,15 @@
+package org.opentutorials.javatutorials.object;
+
+public class Calculator1 {
+	
+	  // refactoring
+	  public static void sum(int left, int right) {
+	        System.out.println(left + right);
+	    }
+	 
+	    public static void main(String[] args) {
+	        sum(10, 20);
+	        sum(20, 40);
+	}
+
+}
